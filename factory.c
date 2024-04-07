811558d4a5344aadea8eabf1976a0b24925e77bb
streylix <paeem@dukes.jmu.edu>
2024-04-06 20:17:00 -0400

    they connect, but fail to communicate

--- factory.c
+++ factory.c
@@ -59,19 +59,22 @@ void goodbye(int sig)
     /* Mission Accomplished */
     printf( "\n### I (%d) have been nicely asked to TERMINATE. "
            "goodbye\n\n" , getpid() );  
-
-    // missing code goes here
-
+    close(sd);
+    exit(0);
 }
 
 /*-------------------------------------------------------*/
 int main( int argc , char *argv[] )
 {
-    char  *myName = "Ethan, Gillian" ; 
+    char  *myName = "Ethan Pae, Gillian Kelly" ; 
     unsigned short port = 50015 ;      /* service port number  */
     int    N = 1 ;                     /* Num threads serving the client */
 
     printf("\nThis is the FACTORY server developed by %s\n\n" , myName ) ;
+
+    signal(SIGINT, goodbye);
+    signal(SIGTERM, goodbye);
+
 	switch (argc) 
 	{
       case 1:
@@ -99,39 +102,58 @@ int main( int argc , char *argv[] )
     // client (implemented by the procurement.c program) 
     // containing the orderSize.
 
+    sd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    memset(&srvrSkt, 0, sizeof(srvrSkt));
+    srvrSkt.sin_family = AF_INET;
+    srvrSkt.sin_addr.s_addr = htonl(INADDR_ANY);
+    srvrSkt.sin_port = htons(port);
+
+    bind(sd, (SA *)&srvrSkt, sizeof(srvrSkt));
+
+    socklen_t clntLen = sizeof(clntSkt);
+    msgBuf msg1;
 
     int forever = 1;
     while ( forever )
     {
         printf( "\nFACTORY server waiting for Order Requests\n" ) ; 
 
-        // missing code goes here
+        recvfrom(sd, &msg1, sizeof(msg1), 0, (SA *)&clntSkt, &clntLen);
 
-        // immediately responds to that client with a ORDR_CONFIRM
-        // message over UDPcontaining the number N of sub-factories
-        // that will manufacture the requested order. For this project,
-        // N is always 1. The server initializes the remainsToMake to be
-        // the same as the orderSize.
+        if (msg1.purpose == REQUEST_MSG){
+            remainsToMake = msg1.orderSize;
+
+            msg1.purpose = ORDR_CONFIRM;
+            msg1.numFac = N;
+            sendto(sd, &msg1, sizeof(msg1), 0, (SA *)&clntSkt, clntLen);
+
+            // immediately responds to that client with a ORDR_CONFIRM
+            // message over UDPcontaining the number N of sub-factories
+            // that will manufacture the requested order. For this project,
+            // N is always 1. The server initializes the remainsToMake to be
+            // the same as the orderSize.
 
 
-        printf("\n\nFACTORY server received: " ) ;
-        printMsg( & msg1 );  puts("");
+            printf("\n\nFACTORY server received: " ) ;
+            printMsg( & msg1 );  puts("");
 
-        // missing code goes here
+            // missing code goes here
 
-        // Activates a single sub-factory, implemented as a regular 
-        // function() call in factory.c , providing it with its 
-        // FactoryID ( 1 for this PA ), its capacity (say 50 items), 
-        // and the duration in milliseconds to make that capacity 
-        // (or part of) of items.
+            // Activates a single sub-factory, implemented as a regular 
+            // function() call in factory.c , providing it with its 
+            // FactoryID ( 1 for this PA ), its capacity (say 50 items), 
+            // and the duration in milliseconds to make that capacity 
+            // (or part of) of items.
 
-        // The factory server should capture any signals meant to INTerrupt
-        // or TERMinate the server in the proper way.
+            // The factory server should capture any signals meant to INTerrupt
+            // or TERMinate the server in the proper way.
 
-        printf("\n\nFACTORY sent this Order Confirmation to the client " );
-        printMsg(  & msg1 );  puts("");
-        
-        subFactory( 1 , 50 , 350 ) ;  // Single factory, ID=1 , capacity=50, duration=350 msg
+            printf("\n\nFACTORY sent this Order Confirmation to the client " );
+            printMsg(  & msg1 );  puts("");
+            
+            subFactory( 1 , 50 , 350 ) ;  // Single factory, ID=1 , capacity=50, duration=350 msg
+        }
     }
 
 
@@ -143,52 +165,41 @@ void subFactory( int factoryID , int myCapacity , int myDuration )
     char    strBuff[ MAXSTR ] ;   // print buffer
     int     partsImade = 0 , myIterations = 0 ;
     msgBuf  msg;
+    int toMake;
 
+    // The sub-factory,  as long as ( remainsToMake  > 0  )
     while ( 1 )
     {
         // See if there are still any parts to manufacture
         if ( remainsToMake <= 0 )
             break ;   // Not anymore, exit the loop
         
-
-
-        // missing code goes here
-
-        // The sub-factory,  as long as ( remainsToMake  > 0  ) , 
-        // it repeatedly:
-            // -Decides how many to make as the lesser of capacity 
-            // and remainsToMake
-            // -Decrements remainsToMake  by the number of items this 
-            // sub-factory will make,
-            // -Sleeps for duration milliseconds, to emulate the actual 
-            // manufacturing of the items,
-
-
+        toMake = minimum(myCapacity, remainsToMake); // -Decides how many to make as the lesser of capacity and remainsToMake
+        remainsToMake -= toMake;                     // -Decrements remainsToMake  by the number of items this sub-factory will make,
+        usleep(myDuration * 1000);                   // -Sleeps for duration milliseconds, to emulate the actual 
+                                                     // manufacturing of the items,
+        partsImade += toMake;
+        myIterations++;
 
         // Send a Production Message to Supervisor
 
-            // -After waking up, the sub-factory sends a  PRODUCTION_MSG 
-            // message over UDP containing its FactoryID  , its capacity, 
-            // the actual number of items it has just made in this 
-            // iteration, and the duration in milliseconds it took to make
-            // that many items.
+        msg.purpose = PRODUCTION_MSG;    // -After waking up, the sub-factory sends a  PRODUCTION_MSG 
+        msg.facID = factoryID;           // message over UDP containing its FactoryID
+        msg.capacity = myCapacity;       // its capacity, 
+        msg.partsMade = toMake;          // the actual number of items it has just made in this 
+        msg.duration = myDuration;       // iteration, and the duration in milliseconds it took to make that many items.
     
-        // missing code goes here
+        sendmsg(sd, &msg, sizeof(msg));
 
+        printf("Factory #%d made %d parts in this iteration.\n", factoryID, toMake);
     }
 
     // Send a Completion Message to Supervisor
-    
-    // Once there are no more items to make, the sub-factory sends 
-    // a COMPLETION_MSG message over UDP to the client containing 
-    // its FactoryID.
+    msg.purpose = COMPLETION_MSG;    // Once there are no more items to make, the sub-factory sends 
+    msg.facID = factoryID;           // a COMPLETION_MSG message over UDP to the client containing
+                                     // its FactoryID. 
 
-    // missing code goes here
-
-    // The sub-factory keeps track of how many iterations it has
-    // performed, and the total number of items it has has actually
-    // made. It prints this information to the screen.    
-
+    sendmsg(sd, &msg, sizeof(msg));
 
     snprintf( strBuff , MAXSTR , ">>> Factory # %-3d: Terminating after making total of %-5d parts in %-4d iterations\n" 
           , factoryID, partsImade, myIterations);
--- procurement.c
+++ procurement.c
@@ -34,7 +34,7 @@ int main( int argc , char *argv[] )
             iters[ MAXFACTORIES+1 ] = {0} ,  // num Iterations completed by each Factory
             partsMade[ MAXFACTORIES+1 ] = {0} , totalItems = 0;
 
-    char  *myName = "Replace with your Names" ; 
+    char  *myName = "Ethan Pae, Gillian Kelly" ; 
     printf("\nPROCUREMENT: Started. Developed by %s\n\n" , myName );    
     fflush( stdout ) ;
     
@@ -50,28 +50,24 @@ int main( int argc , char *argv[] )
  
 
     /* Set up local and remote sockets */
+    int sockfd;
 
+    struct sockaddr_in servaddr;
 
-    // missing code goes here
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
-        // Gets the order size , the server's IP address 
-        // (in dotted-decimal format) , and the server's port number
-        // via the command-lie arguments.
-
-
-    // Prepare the server's socket address structure
-    
-    // missing code goes here
-
+    memset(&servaddr, 0, sizeof(servaddr));        // Gets the order size , the server's IP address
+    servaddr.sin_family = AF_INET;                 // (in dotted-decimal format) , and the server's port number
+    servaddr.sin_port = htons(port);               // via the command-lie arguments.
+    inet_pton(AF_INET, serverIP, &servaddr.sin_addr);
 
     // Send the initial request to the Factory Server
     msgBuf  msg1;
 
-    // missing code goes here
+    msg1.purpose = REQUEST_MSG; // Sends a REQUEST_MSG message over UDP to the 
+    msg1.orderSize = orderSize; // factory server containing the order size.
 
-        // Sends a REQUEST_MSG message over UDP to the 
-        // factory server containing the order size.
-
+    sendto(sockfd, &msg1, sizeof(msg1), 0, (SA *)&servaddr, sizeof(servaddr));
 
     printf("\nPROCUREMENT Sent this message to the FACTORY server: "  );
     printMsg( & msg1 );  puts("");
@@ -81,57 +77,56 @@ int main( int argc , char *argv[] )
     msgBuf  msg2;
     printf ("\nPROCUREMENT is now waiting for order confirmation ...\n" );
 
+    // Awaits the ORDR_CONFIRM message over UDP
+    socklen_t len = sizeof(servaddr);
+    recvfrom(sockfd, &msg2, sizeof(msg2), 0, (SA *)&servaddr, &len);
 
-    // missing code goes here
-
-        // Awaits the ORDR_CONFIRM message over UDP
-
+    if (msg2.purpose == ORDR_CONFIRM){
+        numFactories = 1;
+        activeFactories = 1; // it will only be one factory for this PA
+    }
 
     printf("PROCUREMENT received this from the FACTORY server: "  );
     printMsg( & msg2 );  puts("\n");
 
-
-    // missing code goes here
-
-        // learns how many sub-factories will be actively serving
-        // it (in this PA, it's always 1 sub-factory)
-
+    // learns how many sub-factories will be actively serving it (which will be 1)
     // Monitor all Active Factory Lines & Collect Production Reports
-    while ( activeFactories > 0 ) // wait for messages from sub-factories
-    {
+    while (activeFactories > 0){    // wait for messages from sub-factories
+        recvfrom(sockfd, &msg2, sizeof(msg2), 0, (SA *)&servaddr, &len); 
 
-
-        // missing code goes here
-            // awaits for a message over UDP        
-
-       // Inspect the incoming message
-
-
-       // missing code goes here
-            // If it is a PRODUCTION_MSG message, print it and 
-            // update the totals ( iterations and items made) 
-            // for the sub-factory that sent the production message.
-
-            // If it is a  COMPLETION_MSG message, decrement the 
-            // number of active sub-factories
-
+        // awaits for a message over UDP
+        // Inspect the incoming message
+        if (msg2.purpose == PRODUCTION_MSG){
+            printMsg(&msg2);                        // If it is a PRODUCTION_MSG message, print it and 
+            iters[msg2.facID]++;                    // update the totals ( iterations and items made) 
+            partsMade[msg2.facID] += msg2.partsMade;// for the sub-factory that sent the production message.
+            totalItems += msg2.partsMade;
+        } else if (msg2.purpose == COMPLETION_MSG) {// If it is a  COMPLETION_MSG message, decrement the 
+            activeFactories--;                      // number of active sub-factories
+        }
     } 
 
     // Print the summary report
     totalItems  = 0 ;
     printf("\n\n****** PROCUREMENT Summary Report ******\n");
 
+    for (int i = 1; i <= numFactories; i++){
+        printf("Factory %d: Iterations: %d, Parts Made: %d\n", i, iters[i], partsMade[i]);
+    }
+    printf("Total Items Made: %d\n", totalItems);
 
-    // missing code goes here
         // When there are no more active sub-factories, the client
         // prints a summary report showing the total items made, 
         // and iterations performed, by each sub-factory.
 
-
     printf("==============================\n") ;
 
+    if (totalItems == orderSize){
+        printf("Order Fulfilled.\n");
+    } else {
+        printf("Order Not Fulfilled.\n");
+    }
 
-    // missing code goes here
         // The clients also compares the total items made by all 
         // sub-factories against the order size it 
         // originally placed.
@@ -139,7 +134,8 @@ int main( int argc , char *argv[] )
 
     printf( "\n>>> Supervisor Terminated\n");
 
-    // missing code goes here
+    close(sockfd);
+
         // Close and terminate any remaining processes
 
     return 0 ;
